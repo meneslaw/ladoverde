1f22fdc6e8e780857db3b8e7b8ac023750ef33e7
meneslaw <89371931+meneslaw@users.noreply.github.com>
2023-10-05 17:57:40 -0300

    all
    full upgrade

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,3 +74,72 @@ function start() {
 }
 
 window.addEventListener("load", start)
+const pixels = document.querySelector('#emailContact').style.borderLeftWidth = "10px"
+const solid = document.querySelector('#emailContact').style.borderLeftStyle = "solid"
+function focusOutForm(str, num){
+    //document.querySelector('#'+str).style.borderLeft = "";
+    var strForm = document.getElementById(str).value;
+    var numForm = num;
+    if(strForm.length >= numForm){
+        if(str =="nameContact"){
+            document.getElementById("clientForm").innerHTML = strForm;
+        }
+        document.querySelector('#'+str).style.borderLeftColor = "#9ffaa3";
+    }else{
+        pixels;
+        solid;
+        document.querySelector('#'+str).style.borderColor = "Tomato";
+    }
+
+    //console.log(document.getElementById("clientForm"))
+}
+function emailFormRegex(){
+    
+    pixels;
+    solid;
+    var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var emailDigitado = document.getElementById("emailContact").value;
+    if(emailDigitado.match(emailRegex)){
+        
+        document.querySelector('#emailContact').style.borderLeftColor = "#9ffaa3";
+    }
+    else
+    {
+        document.querySelector('#emailContact').style.borderLeftColor = "Tomato";
+    }
+}
+var form = document.getElementById('formItems');
+
+form.addEventListener('focus', (event) => {
+  event.target.style.borderLeftColor = "gold";
+}, true);
+
+form.addEventListener('blur', (event) => {
+    var erros = document.querySelector('#errorsSpan');
+  event.target.style.borderLeftColor = "";
+  var inputName = event.target.name.split(" ");
+  var inputValue = event.target.value;
+ 
+  if(inputValue.length < inputName[0]){
+   
+    if(document.querySelectorAll("#"+inputName[1]).length ==0){
+        var criaP = document.createElement("p");
+        criaP.setAttribute("id",inputName[1]);
+        erros.appendChild(criaP);
+        document.getElementById(inputName[1]).innerHTML = "O Campo "+inputName[1]+" precisa ter ao menos "+inputName[0];
+    }
+  }else{
+    const dados = document.querySelectorAll("#"+inputName[1]);
+    for(var i=0; i < dados.length; i++){
+        var sys = dados[i];
+        sys.remove()
+    }
+  }
+}, true);
+form.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    var inp = event.target.elements;
+    for(var i=0; i < inp.length; i++){
+        console.log(inp[i].value)
+    }
+}, true);
