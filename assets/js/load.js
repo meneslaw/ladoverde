8bfec1e91fd95949a5e2816342358faab53d749b
meneslaw <89371931+meneslaw@users.noreply.github.com>
2023-10-10 03:56:53 -0300

    Add files via upload
    animations and json "bd"

--- assets/js/load.js
+++ assets/js/load.js
@@ -20,12 +20,29 @@ const modalHref = document.getElementsByTagName("section")[0];
 
 modalHref.addEventListener('click', (event) => {
     if (event.target.classList == "showServicesModal") {
+        const animationModal = document.getElementById("modalDialog");
         const modalDialogTitle = document.querySelector(".modalTitle");
         modalDialogTitle.innerHTML = event.target.textContent;
         lerTxt(event.target.textContent);
+        animationModal.className = "animationModal";
     } else {
         console.log("clicou em qualquer lugar")
     }
 
 
 }, true);
+function xHide() {
+    document.getElementById("modalDialog").className = "animationModalClose";
+
+    const eleX = document.getElementById("modalDialog");
+
+    window.addEventListener('animationend', (event) => {
+        if (document.getElementById("modalDialog").className == "animationModalClose") {
+            console.log("close");
+            document.getElementsByClassName("button-close")[0].click();
+        }
+        //document.getElementsByClassName("button-close")[0].click();
+        console.log(event)
+        //document.getElementById("modalDialog").className = "animationModal";
+    }, true);
+}
