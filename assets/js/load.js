37d93f97198112bf3a52c587f793752c084e36a4
meneslaw <89371931+meneslaw@users.noreply.github.com>
2023-10-10 22:59:28 -0300

    Add files via upload

--- assets/js/load.js
+++ assets/js/load.js
@@ -46,3 +46,30 @@ function xHide() {
         //document.getElementById("modalDialog").className = "animationModal";
     }, true);
 }
+let CreateDOMObjects = () => {
+    const DOM = `
+        <div vw class="enabled">
+        <div vw-access-button class="active"></div>
+        <div vw-plugin-wrapper>
+            <div class="vw-plugin-top-wrapper"></div>
+        </div>
+        </div>
+    `;
+    document.body.insertAdjacentHTML('beforeend', DOM);
+}
+
+let ImportScriptFile = () => {
+    let script = document.createElement('script');
+    script.src = 'https://vlibras.gov.br/app/vlibras-plugin.js';
+    script.onload = () => {
+        new window.VLibras.Widget('https://vlibras.gov.br/app');
+    };
+    document.head.appendChild(script)
+}
+
+(() => {
+    window.addEventListener('DOMContentLoaded', e => {
+        CreateDOMObjects();
+        ImportScriptFile();
+    });
+})();
